0b537caf5cc770315ba3eef8c893206ee04609f2
Andrey Semenov <semenov.code@gmail.com>
2025-01-26 02:15:25 +0300

    add brain-even.js

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 
 ### Code Climate status:
 [![Maintainability](https://api.codeclimate.com/v1/badges/4fb116ae924b77d594c9/maintainability)](https://codeclimate.com/github/orc-code/frontend-project-44/maintainability)
+
+#### Установка
+1. Клонируем [репозиторий](https://github.com/orc-code/frontend-project-44.git)
+```properties
+git clone https://github.com/orc-code/frontend-project-44.git
+```
+2. Переходим в директорию проекта
+```properties
+cd frontend-project-44/
+```
+3. Устанавливаем зависимости
+```properties
+make install
+```
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,18 +1,26 @@
+#!/usr/bin/env node
+
 import readlineSync from 'readline-sync';
 import greeting from '../src/cli.js';
 
 greeting();
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-const game = () => {
+const game = (roundCount = 1) => {
   const num = Math.floor(Math.random() * 100);
   const answer = readlineSync.question(`Question: ${num}\nYour answer: `);
   const correctAnswer = num % 2 === 0 ? 'yes' : 'no';
   if (answer === correctAnswer) {
     console.log('Correct!');
+    if (roundCount < 3) {
+      game(roundCount + 1);
+    }
+    if (roundCount === 3) {
+      console.log('Congratulations, you win!');
+    }
   } else {
     console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log(`Let's try again, ${readlineSync.question('May I have your name? ')}!`);
+    console.log(`Let's try again!`);
   }
 };
 
